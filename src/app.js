282bef5c78b84eb36d66891d0d89e095f38a8ecc
Carolina-Mattoso <mattosouni@gmail.com>
2023-06-07 16:46:46 -0300

    Date function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} at ${hours}:${minutes}`;
+}
+
+
 
 function displayTemperature(response) {
     document.querySelector("#current-temperature").innerHTML = ` ${Math.round(response.data.temperature.current)}º`;
@@ -6,7 +22,7 @@ function displayTemperature(response) {
     document.querySelector("#feels-like").innerHTML = `Feels like: ${Math.round(response.data.temperature.feels_like)}º`;
     document.querySelector("#humidity").innerHTML = `Humidity: ${(response.data.temperature.humidity)}%`;
     document.querySelector("#wind").innerHTML = `Wind: ${Math.round(response.data.wind.speed)} Km/H`;
-    console.log(response.data);
+    document.querySelector("#last-updated").innerHTML = `Last updated: ${formatDate(response.data.time * 1000)}`;
 }
 
 let apiKey = "3oa2c4504acf0t98cfbd9f4b11f24965";
