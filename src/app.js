14b976d3418a52ef0e3bfc1d2984b16b29c9e241
Carolina-Mattoso <mattosouni@gmail.com>
2023-06-16 13:32:13 -0300

    Removed imperial conversion

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ function formatDate(timestamp){
     let day = days[date.getDay()];
     return `${day} at ${hours}:${minutes}`;
 }
-
 function formatForecastDate(timestamp){
  let date = new Date(timestamp * 1000);
  let day = date.getDay();
@@ -34,7 +33,6 @@ function formatForecastDate(timestamp){
  ];
  return weekDays[day];
 }
-
 function displayForecast(response) {
     let forecast = response.data.daily;
     let forecastElement =  document.querySelector("#forecast");
@@ -58,7 +56,6 @@ forecastHTML = forecastHTML + `</ul>`;
 forecastElement.innerHTML = forecastHTML;
 
 }
-
 function getforecast(currentCity) {
     let apiKey = "3oa2c4504acf0t98cfbd9f4b11f24965";
     let endPoint = "https://api.shecodes.io/weather/v1/forecast?";
@@ -66,7 +63,6 @@ function getforecast(currentCity) {
     let apiUrl = `${endPoint}query=${currentCity}&key=${apiKey}&units${unit}`;
     axios.get(apiUrl).then(displayForecast);
 }
-
 function displayTemperature(response) {
     document.querySelector("#current-temperature").innerHTML = ` ${Math.round(response.data.temperature.current)}º`;
     document.querySelector("#city").innerHTML = (response.data.city);
@@ -80,7 +76,6 @@ function displayTemperature(response) {
       celsiusTemperature = response.data.temperature.current;
       getforecast(response.data.city);
     }
-
 function searchCity(city) {
 let apiKey = "3oa2c4504acf0t98cfbd9f4b11f24965";
 let endPoint = "https://api.shecodes.io/weather/v1/current?";
@@ -90,35 +85,12 @@ axios.get(apiUrl).then(displayTemperature);
 
 
 }
-
 function handleSubmit(event){
     event.preventDefault()
     let cityInput = document.querySelector("#city-input");
     searchCity(cityInput.value);
 }
-function displayFahrenheitTemperature(event){
-    event.preventDefault();
-    let fahrenheitTemperature = (celsiusTemperature * 9)/5 + 32; 
-    document.querySelector("#current-temperature").innerHTML = Math.round(fahrenheitTemperature);
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-}
-function displayCelsiusTemperature(event){
-    event.preventDefault();
-    document.querySelector("#current-temperature").innerHTML = Math.round(celsiusTemperature);
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-}
-
-
-
-let celsiusTemperature = null;
 
 document.getElementById("search-form").addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("São Paulo");
